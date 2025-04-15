03277be186e6857d7e9fe338d4c7f71e821f95ae
RuselUc <russelarielucpuc@gmail.com>
2025-04-15 13:10:37 -0600

    feat: add CircleButton integration to Controls component for enhanced user interaction

--- src/components/layout/Controls.tsx
+++ src/components/layout/Controls.tsx
@@ -5,6 +5,7 @@ import {
   FaAngleUp,
 } from "react-icons/fa";
 import ArrowButton from "./ArrowButton";
+import CircleButton from "./CircleButton";
 
 const buttons = [
   {
@@ -28,19 +29,49 @@ const buttons = [
     icon: FaAngleRight,
   },
 ];
+
+const circleButtons = [
+  {
+    id: "a",
+    classButton: "bg-yellow-500",
+    classContainer: "-translate-y-6",
+    textButton: "A",
+  },
+  {
+    id: "b",
+    classButton: "bg-primary",
+    classContainer: "translate-y-6",
+    textButton: "B",
+  },
+];
 const Controls = () => {
   return (
-    <div className="relative flex items-center w-40 h-40 justify-center">
-      <div className="bg-blue-pokemon h-32 w-32 rounded-full border-4 absolute" />
+    <div className="flex justify-between items-center">
+      <div className="relative flex items-center w-40 h-40 justify-center">
+        <div className="bg-blue-pokemon h-32 w-32 rounded-full border-4 absolute" />
 
-      {buttons.map(({ id, icon: Icon, className }) => (
-        <ArrowButton key={id} className={className}>
-          <Icon className="w-8 h-8" />
-        </ArrowButton>
-      ))}
+        {buttons.map(({ id, icon: Icon, className }) => (
+          <ArrowButton key={id} className={className}>
+            <Icon className="w-8 h-8" />
+          </ArrowButton>
+        ))}
 
-      <div className="bg-white w-16 h-16 absolute"></div>
-      <div className="absolute w-10 h-10 rounded-full border-4"></div>
+        <div className="bg-white w-16 h-16 absolute"></div>
+        <div className="absolute w-10 h-10 rounded-full border-4"></div>
+      </div>
+
+      <div className="flex flex-row-reverse gap-4">
+        {circleButtons.map(
+          ({ id, classButton, classContainer, textButton }) => (
+            <CircleButton
+              key={id}
+              classButton={classButton}
+              classContainer={classContainer}
+              textButton={textButton}
+            />
+          )
+        )}
+      </div>
     </div>
   );
 };
