cde1050bc568bdbf3b0aa58e437aea8bacef8cff
RuselUc <russelarielucpuc@gmail.com>
2025-04-15 17:36:50 -0600

    feat: add Home component and update page to render it

---
+++ src/components/home/index.tsx
@@ -0,0 +1,17 @@
+const Home = () => {
+  return (
+    <div className="font-jersey text-4xl uppercase flex flex-col gap-4">
+      <span className="text-center">
+        Team <br /> builder
+      </span>
+      <span className="font-jaro text-5xl text-yellow-400">Pokemon</span>
+
+      <button className="bg-primary p-1 pixel">
+        <div className="bg-black text-yellow-400 uppercase text-3xl">
+          iniciar
+        </div>
+      </button>
+    </div>
+  );
+};
+export default Home;
